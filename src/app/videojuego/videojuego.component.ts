3eae664e604c7bff038adc25c76e80480035f2db
Carlos <skhcri@gmail.com>
2021-04-06 12:42:57 +0200

    Interpolación de componentes

---
+++ src/app/videojuego/videojuego.component.ts
@@ -0,0 +1,19 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'videojuego',
+    templateUrl: 'videojuego.component.html',
+    styleUrls: []
+})
+
+export class VideojuegoComponent {
+    public titulo: string;
+    public listado: string;
+    
+    constructor(){
+        this.titulo = "Componente de videojuegos";
+        this.listado = "Listado de juegos";
+        
+        console.log("Se ha cargado el componente: VideojuegoComponent");
+    }
+}
