1912831e1f800416f2c1ebec9d27e2ef360e1e4e
Carlos <skhcri@gmail.com>
2021-04-07 10:50:29 +0200

    ngFor y estilos condicionales

--- src/app/zapatillas/zapatillas.component.ts
+++ src/app/zapatillas/zapatillas.component.ts
@@ -9,16 +9,31 @@ import {Zapatilla} from "../models/Zapatilla";
 export class ZapatillasComponent implements OnInit{
     public titulo: string = "Componente de zapatillas";
     public zapatillas: Array<Zapatilla>;
+    public marcas: String[];
     
     constructor(){
+        this.marcas = new Array();
         this.zapatillas = [
+            new Zapatilla('Nike Red',"Nike","Rojo",90,true),
             new Zapatilla('Reebook classic',"Reebok","Blanco",50,false),
-            new Zapatilla('Nike runner',"Nike","Negro",90,false),
-            new Zapatilla('Adidas grey',"Adidas","Griss",250,true)
+            new Zapatilla('Nike runner',"Nike","Negro",190,false),
+            new Zapatilla('Adidas grey',"Adidas","Gris",250,true),
+            new Zapatilla('Reebook Strong',"Reebok","Verde",30,false)
         ];
     }
 
     ngOnInit(){
         console.log(this.zapatillas);
+        this.getMarcas();
     }
+
+    getMarcas(){
+        this.zapatillas.forEach((zapatilla, index)=>{
+            if (this.marcas.indexOf(zapatilla.marca)<0) {
+                this.marcas.push(zapatilla.marca);
+            }
+        });
+        console.log(this.marcas);
+    }
+
 }
