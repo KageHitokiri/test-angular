a578d9f68502c22294146aa3890a8fa041841a8c
Carlos <skhcri@gmail.com>
2021-04-08 11:21:51 +0200

    Enrutamiento de la página

---
+++ src/app/app.routing.ts
@@ -0,0 +1,23 @@
+// Importar módulos del router de angular
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router'
+
+// Importar componentes
+
+import { ZapatillasComponent } from './zapatillas/zapatillas.component';
+import { VideojuegoComponent } from './videojuego/videojuego.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { HomeComponent } from './home/home.component';
+import { Route } from '@angular/compiler/src/core';
+
+// Array de rutas
+const appRoutes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'zapatillas', component: ZapatillasComponent},
+    {path: 'videojuego', component: VideojuegoComponent},
+    {path: 'cursos', component: CursosComponent},
+    {path: '**', component: HomeComponent}
+];
+
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders<Route> = RouterModule.forRoot(appRoutes);
